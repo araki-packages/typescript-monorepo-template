c9b32bc42d44dec680a976a32171fb7d051e467e
ArakiTakaki <araki_takaki@yahoo.co.jp>
2020-03-29 10:58:06 +0900

    ok generate

--- scripts/rollup/generator.js
+++ scripts/rollup/generator.js
@@ -6,7 +6,8 @@ const typescript = require('@rollup/plugin-typescript');
 const DEVICE = require('./device');
 const path = require('path');
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
-const generateRollupConfig = (options) => ({
+
+const generateRollupInput = (options) => ({
   input: options.input,
   plugins: [
     resolve({ extensions }),
@@ -20,47 +21,41 @@ const generateRollupConfig = (options) => ({
     const isNodeModules = !(/\.+\//.test(id));
     const ignore = options.ignoreNodeModules ? isNodeModules : false;
     if (ignore) {
-      console.log(chalk.bgRed(`-- ${id} `));
+      console.log(`-- ${id} `);
     }else {
-      console.log(chalk.bgCyan(`++ ${id} `));
+      console.log(`++ ${id} `);
     }
     return ignore;
   },
-  output: [{
-    file: 'dist/main.cjs.js',
-    format: 'cjs'
-  }, {
-    file: 'dist/main.es.js',
-    format: 'es',
-  }]
 });
 
-const build = (config) => {
-  // TODO 
-  // outputoptions ref->  https://rollupjs.org/guide/en/#outputoptions-object
-  rollup.rollup(config)
-    .then((value) => {
-      console.log('success');
-      console.log(value);
-      value.write();
-    })
-    .catch((err) => {
-      console.log('fail');
-      throw err;
-    });
+const generateRollupOutputList = (options) => {
+  return [{
+    file: path.resolve(options.projectRoot, 'dist', 'main.cjs.js'),
+    format: 'cjs'
+  }, {
+    file: path.resolve(options.projectRoot, 'dist', 'main.esm.js'),
+    format: 'es',
+  }];
 };
 
 (() => {
   
   const packageEntries = DEVICE.PACKAGES.map((package) =>  path.resolve(DEVICE.PROJECT_ROOT, 'packages', package, 'index.ts'));
-  console.log(packageEntries);
-  packageEntries.forEach((value) => {
-    build(generateRollupConfig({
-      input: value,
-      ignoreNodeModules: true
-    }));
+  packageEntries.forEach(async (value) => {
+    try{
+      const build = await rollup.rollup(generateRollupInput({
+        input: value,
+        ignoreNodeModules: true
+      }));
+      generateRollupOutputList({
+        projectRoot: path.dirname(value),
+      })
+        .forEach((output) => {
+          build.write(output);
+        });
+    } catch (err) {
+      throw err;
+    }
   })
-  
-
-  
 })();
